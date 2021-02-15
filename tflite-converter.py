f1494c08509ceace3fa599ca5616e0d7fb11da3c
ferhatyaman <ferhatyaman@sabanciuniv.edu>
2021-02-15 11:03:34 -0500

    first commit

---
+++ tflite-converter.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8  # or tf.uint8
+converter.inference_output_type = tf.int8  # or tf.uint8
+tflite_quant_model = converter.convert()
